26f7c85acf23e75c3d13be33f1ec0c1efe6a0027
jeffersonklamas <jeffersonklamas@gmail.com>
2021-04-20 01:06:53 -0300

    Criando mensageiro para whatsapp

---
+++ inicioestudos/Whatsapp.py
@@ -0,0 +1,7 @@
+# Enviar mensagem para Whatsapp
+# instale via pip o pacote pywhatkit
+
+import pywhatkit as Whatsapp
+
+#Enviando mensagem para o número
+Whatsapp.sendwhatmsg('+5541999548681', 'Olá Abençoado ;)',0,0)
